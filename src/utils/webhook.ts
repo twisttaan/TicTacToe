ee1267bcd163bded028b82d9a2905a48bf7bb432
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-03-24 20:01:49 -0500

    feat: Store error webhook in env (smh)

--- src/utils/webhook.ts
+++ src/utils/webhook.ts
@@ -5,7 +5,7 @@ export async function reportToWebhook(
   error: string,
 ): Promise<AxiosResponse<APIWebhook>> {
   return await axios.post(
-    `https://canary.discord.com/api/webhooks/955007137128529950/m6fOF6urzRGRhY_Yt12vLjc51U2b5IdN5Hv5Fy463GSMJzonr-Xwiq8H-yOLoR8QzU_9`,
+    process.env.ERROR_WEBHOOK
     {
       content: error,
     },
